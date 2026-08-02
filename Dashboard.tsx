7ab476a290ebadc5a7a02236fb96fa2738312046
mrbahar1984-cpu <mr.bahar1984@gmail.com>
2026-08-02 17:30:28 +0330

    Delete Dashboard.tsx

--- Dashboard.tsx
+++
@@ -1,4 +0,0 @@
-
-import React from 'react';
-const OldDashboard = () => <div className="p-10 text-center font-bold">این نسخه قدیمی است. لطفا از روت اصلی استفاده کنید.</div>;
-export default OldDashboard;
